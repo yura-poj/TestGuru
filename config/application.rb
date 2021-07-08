9f8702353e8a97f800e941944832e0970af40b2d
yura-poj <85958170+yura-poj@users.noreply.github.com>
2021-07-08 22:20:19 +0700

    change configs

--- config/application.rb
+++ config/application.rb
@@ -17,8 +17,7 @@ module TestGuru
     # in config/environments, which are processed later.
     #
     config.time_zone = "Asia/Novokuznetsk"
-    config.location = "Kemerovo"
-    config.language = "Russian"
+    config.i18n.default_locale = :ru
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
